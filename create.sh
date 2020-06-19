fd3fecd3b754cd1a2137a1d9a6d05a1fb55117bc
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-06-19 10:39:43 +0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -3,7 +3,7 @@ MORE=`cat site`
 git config --global user.email "support@download-site.com"
 git config --global user.name "Site Downloader"
 wget -r -l1 -k -p --restrict-file-names=ascii --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0" http://$MORE
-echo [$MORE]'(https://kamakepar2029.github.io/Site-downloader/'$MORE'/)'>>README.md
+echo '#'[$MORE]'(https://kamakepar2029.github.io/Site-downloader/'$MORE'/)'>>README.md
 git add .
 git commit -m $MORE
 git push -u origin master
