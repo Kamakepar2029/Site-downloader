42e1ba00a1c517f12ead09a39c89a47a06fe8ae3
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-06-19 10:14:58 +0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
 wget -r -l1 -k -p --restrict-file-names=ascii --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0" http://$1
 git init
-git add *
+git add .
 git commit -m $1
 git push -u origin master
