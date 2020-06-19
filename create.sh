28364b9850dbec7cacaabcf6074b84c90fd7c2a3
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-06-19 10:25:05 +0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,7 +1,8 @@
 git init
+MORE=`cat site`
 git config --global user.email "support@download-site.com"
 git config --global user.name "Site Downloader"
-wget -r -l1 -k -p --restrict-file-names=ascii --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0" http://$1
+wget -r -l1 -k -p --restrict-file-names=ascii --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0" http://$MORE
 git add .
 git commit -m $1
 git push -u origin master
