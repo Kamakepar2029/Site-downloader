3915ec50f86a3c6a95eddef5c077c99d0c5947f9
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-06-19 16:59:15 +0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -2,7 +2,7 @@ git init
 MORE=`cat site`
 git config --global user.email "support@download-site.com"
 git config --global user.name "Site Downloader"
-wget -r -l1 -k -p --restrict-file-names=ascii --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0" http://$MORE
+#wget -r -l1 -k -p --restrict-file-names=ascii --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0" http://$MORE
 wget \
      --recursive \
      --no-clobber \
