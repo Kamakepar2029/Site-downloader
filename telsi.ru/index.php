46a1acd8ae76f8d6c03b976e81ca1e642cdc0e86
Site Downloader <support@download-site.com>
2020-06-19 07:20:44 +0000

    telsi.ru

---
+++ telsi.ru/index.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta http-equiv="pragma" content="no-cache">
+<title>Телси </title>
+<meta name="description" content="СИСТЕМЫ ДИСПЕТЧЕРСКОЙ СВЯЗИ И ВЫЗОВА ПЕРСОНАЛА">
+<meta name="keywords" content="диспетчерская связь для лифта, директорская связь, связь для лифтов, палатная сигнализация">
+
+<link href="design/telsi_shop/styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+
+<div class="main_table">
+<table  cellpadding="0" cellspacing="0">
+  <tbody>
+    <tr>
+      <td style="width: 397px;" colspan="3" rowspan="1"></td>
+    </tr>
+    <tr style="vertical-align: top;">
+      <td style="width: 300px;">
+	  <!--Левое меню-->
+		  <table width="100%" cellpadding="0" cellspacing="0">
+ <tr class="vMenuHtr"> 
+  <td class="vMenuLeftRound"><img src="design/telsi_shop/img/bg-vmenuhtr-left.gif" width="23" height="32"><td class="vMenuLeftText">Каталог товаров</td><td class="vMenuRightRound"><img src="design/telsi_shop/img/bg-vmenuhtr-right.gif" width="20" height="32"></td>
+ </tr>
+ <tr>
+  <td colspan="3">
+<table cellspacing="0" cellpadding="0" class="MnuCat2"><tr><td width="1%" class="ec1"></td><td class="ec1"><a href="catalog/SISTEMI-VIZOVA-PERSONALA/index.html">СИСТЕМЫ ВЫЗОВА ПЕРСОНАЛА</a><table cellspacing="0" cellpadding="0"></table></td></tr><tr><td width="1%" class="ec1"></td><td class="ec1"><a href="catalog/OBORUDOVANIE-PO-PROGRAMME-DOSTUPNAYA-SREDA/index.html">ОБОРУДОВАНИЕ ПО ПРОГРАММЕ ДОСТУПНАЯ СРЕДА</a><table cellspacing="0" cellpadding="0"></table></td></tr><tr><td width="1%" class="ec1"></td><td class="ec1"><a href="catalog/SISTEMI-OPERATIVNOJ-DISPETCHERSKOJ-SVYAZI-/index.html">СИСТЕМЫ ОПЕРАТИВНОЙ ДИСПЕТЧЕРСКОЙ СВЯЗИ GETCALL</a><table cellspacing="0" cellpadding="0"></table></td></tr><tr><td width="1%" class="ec1"></td><td class="ec1"><a href="catalog/SISTEMA-OPERATIVNOJ-DISPETCHERSKOJ-SVYAZI-/index.html">СИСТЕМА ОПЕРАТИВНОЙ ДИСПЕТЧЕРСКОЙ СВЯЗИ &quot;Регион&quot;</a><table cellspacing="0" cellpadding="0"></table></td></tr><tr><td width="1%" class="ec1"></td><td class="ec1"><a href="catalog/SISTEMI-VIZOVA-POSETITELEJ-V-KABINET/index.html">СИСТЕМЫ ВЫЗОВА ПОСЕТИТЕЛЕЙ В КАБИНЕТ HostCall-CV</a><table cellspacing="0" cellpadding="0"></table></td></tr><tr><td width="1%" class="ec1"></td><td class="ec1"><a href="catalog/SISTEMI-NOCHNOGO-DEZHURNOGO-OSVESHHENIYA-H/index.html">СИСТЕМЫ НОЧНОГО ДЕЖУРНОГО ОСВЕЩЕНИЯ «HostCall ®-LT»</a><table cellspacing="0" cellpadding="0"></table></td></tr><tr><td width="1%" class="ec1"></td><td class="ec1"><a href="catalog/Sistema-ojenki-kachestva-obsluzhivaniya-HostCall-QR/index.html">СИСТЕМА ОЦЕНКИ КАЧЕСТВА ОБСЛУЖИВАНИЯ HostCall-QR</a><table cellspacing="0" cellpadding="0"></table></td></tr><tr><td width="1%" class="ec1"></td><td class="ec1"><a href="catalog/CISTEMA-ELEKTRONNOJ-OCHEREDI-HostCall-QM/index.html">CИСТЕМА ЭЛЕКТРОННОЙ ОЧЕРЕДИ HostCall-QM</a><table cellspacing="0" cellpadding="0"></table></td></tr><tr><td width="1%" class="ec1"></td><td class="ec1"><a href="catalog/Primeri-proektov/index.html">КАТАЛОГ ПРОЕКТОВ</a><table cellspacing="0" cellpadding="0"></table></td></tr><tr><td width="1%" class="ec1"></td><td class="ec1"><a href="catalog/SERTIFIKATI/index.html">СЕРТИФИКАТЫ</a><table cellspacing="0" cellpadding="0"></table></td></tr></table>
+  </td>
+ </tr>
+</table>
+<div class="LineVM"><img src="design/telsi_shop/img/line3-vm.gif" width="97%" height="7"></div>
+<script type="text/javascript">
+var dlinks=document.getElementsByTagName('a');
+ for(var i=0;i<dlinks.length;i++){
+  if(dlinks[i].href==document.location.href){
+  dlinks[i].className='OpenedCat';
+  break;
+  }
+ }
+</script>
+
+
+
+		  
+  		
+ 		
+	  <!--КОНЕЦ Левое меню--></td>
+      <td style="width: 600px;">
+	  		<div class="content">
+
+ 
+
+			<p><p class="MsoNormal" style="line-height: normal; margin-bottom: 0pt;" align="center"><span style="font-family: times new roman, times, serif; font-size: 12pt;">&nbsp;</span></p>
+<p><span style="font-family: times new roman, times, serif; font-size: 12pt;">&nbsp;&nbsp;&nbsp; Настоящий сайт является головным сайтом нашей компании &nbsp;и содержит оглавление каталога предлагаемого нами оборудования и текущие новости компании.</span><br style="font-family: Verdana;" /><span style="font-size: 12pt; font-family: times new roman, times, serif;"> <span style="color: black;">&nbsp;&nbsp;&nbsp; Представленное на сайте оборудование охватывает широкий перечень изделий, нашедших широкое применение на предприятиях и организациях самых разных секторов народного хозяйства нашей страны.<br />&nbsp;&nbsp;&nbsp; Прежде всего, на сайте широко представлены системы вызова персонала и системы вызова посетителей&nbsp; торговой марки &laquo;<strong>HostCall&raquo;, </strong>системы вызова персонала торговой марки &laquo;</span><strong style="font-family: Verdana;">Ge</strong><strong style="font-family: Verdana;">tСall</strong><span style="color: black;">&raquo;, и специализированные системы диспетчерской связи торговой марки &laquo;</span><strong style="font-family: Verdana;">Ge</strong><strong style="font-family: Verdana;">tСall</strong><span style="color: black;">&raquo;. Эти системы были разработаны специалистами нашей компании, и мы их серийно выпускаем. В каталог включено оборудование, разработанное нами для программы <strong>ДОСТУПНАЯ СРЕДА.</strong> Наряду с этим представлены и другие системы и средства связи и коммуникаций: диспетчерской связи торговой марки &laquo;<strong>Регион</strong>&raquo;, системы точного времени.</span><span style="color: black;">&nbsp; </span></span></p>
+<p><span style="font-size: 12pt; line-height: 115%; font-family: times new roman, times, serif; color: black;">&nbsp;&nbsp;&nbsp; C оглавления каталога осуществляется автоматический переход на тематические сайты, на которых представлена подробная информация по всему перечню оборудования входящему в выше перечисленные системы. Сервис тематических сайтов дает возможность заказа оборудования, в том числе, с доставкой по России, выложена соответствующая эксплуатационная и нормативная документация, сертификаты, руководства по проектированию, примеры проектов, тематические статьи.</span></p></p>
+
+
+
+
+
+<p class="MsoNoSpacing"><strong><u>Информационная поддержка клиентов</u></strong></p>
+<p class="MsoNoSpacing">Официальный сайт компании <a href="index.html">www.telsi.ru</a> ;</p>
+<p class="MsoNoSpacing">Тематический сайт компании, посвященный оборудованию профессиональных систем вызова персонала и систем вызова пациентов торговой марки <span lang="EN-US">HOSTCALL</span><span lang="EN-US"> &nbsp;</span><a href="http://www.hostcall.ru/">www.hostcall.ru</a> ;</p>
+<p class="MsoNoSpacing">Тематический сайт компании, посвященный оборудованию специализированных систем диспетчерской связи и переговорных устройств торговой марки <span lang="EN-US">GETCALL</span> <a href="http://www.getcall.ru/">www.<span lang="EN-US">get</span>call.ru</a> ;</p>
+<p class="MsoNoSpacing">Тематический сайт компании, посвященный оборудованию по программе ДОСТУПНАЯ СРЕДА <a href="http://www.multicall.ru/">www.multicall.ru</a> ;</p>
+<p class="MsoNoSpacing">Тематический сайт компании посвященный системам оперативно-диспетчерской связи &ldquo;Регион&rdquo;; системам производственной связи, кроссовому оборудованию, кабелям связи и целому ряду дополнительного оборудования <a href="http://www.service-ats.ru/">www.service-ats.ru</a> ;</p>
+<p class="MsoNoSpacing">Тематический сайт компании, посвященный системам громкого оповещения торговой марки "INTER-M" &nbsp;<a href="http://www.inter-m.name/">www.inter-m.name</a> ;</p>
+<p class="MsoNoSpacing">Тематический сайт компании, в основном посвященный техническим решениям вызова персонала в гостиницах и других средствах размещения &nbsp;<a href="http://www.sks-security.ru/">www.sks-security.ru</a> .</p>
+			</div>
+	  </td>
+      <td style="width: 300px;">
+	  <!--Правое меню-->
+  
+  
+  			<table width="100%" cellpadding="0" cellspacing="0">
+ <tr class="vMenuHtr">
+  <td class="vMenuLeftRound"><img src="design/telsi_shop/img/bg-vmenuhtr-left.gif" width="23" height="32"></td>
+  <td class="vMenuLeftText"><a href="news/index.html">Новости</a></td><td class="vMenuRightRound"><img src="design/telsi_shop/img/bg-vmenuhtr-right.gif" width="20" height="32">
+ </tr>
+ <tr>
+  <td colspan="3">
+   <table cellspacing="0" cellpadding="0" class="MnuCat2">
+    
+    <tr><td>2020-03-30<br><a href="news/Oformlena-deklaratsiya-TR-EAES-037-2016-Ob-ogranichenii-primeneniya-opasnih-veshhestv-v-izdeliyah-elektrotehniki-i-radioelektroniki-na-produktsiyu-marki-HOSTCALL.html">Оформлена  декларация ТР ЕАЭС 037/2016  "Об ограничении применения опасных веществ в изделиях электротехники и радиоэлектроники" на продукцию марки HOSTCALL</a><br></td></tr>
+    
+    <tr><td>2020-03-13<br><a href="news/Novij-pult-dispetcherskoj-svyazi-GC-1001D4.html">Новый пульт диспетчерской связи GC-1001D4.</a><br></td></tr>
+    
+    <tr><td>2020-01-23<br><a href="news/Snyatie-s-proizvodstva-peregovornih-konsolej-KP-02-i-KP-03.html">Снятие с производства переговорных консолей КП-02 и КП-03.</a><br><p>Замена на аналогичную накладную переговорную консоль&nbsp;<a href="http://www.hostcall.ru//catalog/PALATNAYA-SIGNALIZATSIYA-DLYA-VZOVA-PERSON/Sistema-vizova-personala-HOSTCALL-NMNP/Oborudovanie-palati-s-peregovornimi-ustroy/DR-201N-signalno-peregovornoe-ustroystvo.html" target="_blank" rel="noopener">DR-201N</a></p></td></tr>
+    
+    <tr><td>2019-12-23<br><a href="news/Uvazhaemie-klienti-Radi-soobshhit-Vam-grafik-raboti-kompanii-OOO-SKB-Telsi-v-novogodnie-prazdniki-2019-2020-g.html">Уважаемые клиенты! Рады сообщить Вам график работы компании ООО "СКБ Телси" в новогодние праздники 2019-2020 г!</a><br></td></tr>
+    
+    <tr><td>2019-12-17<br><a href="news/Postupil-v-prodazhu-kontroller-peredachi-SMS-soobshhenij-o-vizovah-MP-231G1.html">Поступил в продажу контроллер передачи СМС сообщений о вызовах MP-231G1</a><br></td></tr>
+    
+    <tr><td>2019-11-27<br><a href="news/Priglashaem-Vas-posetit-nash-stend-na-vistavke-zdravoohranenie-2019.html">Приглашаем Вас посетить наш стенд на выставке  здравоохранение 2019!</a><br></td></tr>
+    
+    <tr><td>2019-11-11<br><a href="news/Prekrashhen-vipusk-komplektov-vizova-posetitelej-v-kabinet-RK-02.html">Прекращен выпуск комплектов вызова посетителей в кабинет RK.02</a><br></td></tr>
+    
+    <tr><td>2019-06-14<br><a href="news/Priglashaem-Vas-na-MEZHDUNARODNUYU-VISTAVKU-LIFTOV-I-PODEMNOGO-OBORUDOVANIYA-RUSSIAN-ELEVATOR-WEEK.html">Приглашаем Вас на "МЕЖДУНАРОДНУЮ ВЫСТАВКУ ЛИФТОВ И ПОДЪЕМНОГО ОБОРУДОВАНИЯ RUSSIAN ELEVATOR WEEK"</a><br></td></tr>
+    
+    <tr><td>2019-06-11<br><a href="news/Priglashaem-Vas-posetit-nash-stend-na-vistavke-Integratsiya-2019.html">Приглашаем Вас посетить наш стенд на выставке Интеграция 2019.</a><br></td></tr>
+    
+    <tr><td>2019-05-18<br><a href="news/Novinka-Nalazheno-proizvodstvo-vosmikanalnogo-rele-s-upravleniem-po-seti-Ethernet.html">Новинка! Налажено производство восьмиканального реле с управлением по сети Ethernet.</a><br></td></tr>
+    
+   </table>
+  </td>
+ </tr>
+</table>
+<div class="LineVM"><img src="design/telsi_shop/img/line3-vm.gif" width="97%" height="7"></div>
+
+  			
+  
+  		  <!--КОНЕЦ Правое меню--></td>
+    </tr>
+	  <tr>
+      <td class="podval">
+	  <img border="0" src="design/telsi_shop/img/logo.gif" width="5" height="4">
+	  </td>
+      <td class="podval">
+	  			
+	
+        <div class="social_icon"> 
+        <font size="1" color="white" face="Verdana" align="right">   
+        Мы в социальных сетях: <br> <br> 
+        </font>
+	<a href="whatsapp://send?phone=+79636353744"><img style="border: 0px solid; width: 50px; height: 50px;" src="design/telsi_shop/img/whatsapp.png" alt="whatsapp"></a>
+        <a href="https://www.instagram.com/skbtelsi/"><img style="border: 0px solid ; width: 50px; height: 50px;" alt="instagramm link" src="design/telsi_shop/img/instagram.png"></a>
+        <a href="https://www.youtube.com/channel/UCGEOlgfIeQymlgYmPnKGwtg"><img style="border: 0px solid ; width: 50px; height: 50px;" alt="instagramm link" src="design/telsi_shop/img/youtube.png"></a>
+	<a href="https://twitter.com/skbtelsi"><img style="border: 0px solid ; width: 50px; height: 50px;" alt="instagramm link" src="design/telsi_shop/img/twitter.png"></a>
+	</div>
+	</font>
+
+</div>
+	  
+	  </td>
+      <td class="podval"><!--счетчики разные-->
+
+ <!--begin of Top100-->
+ <a href="http://top100.rambler.ru/top100/">
+ <img src="http://counter.rambler.ru/top100.cnt?166485" alt="Rambler's Top100 Service" border="0"></a>
+ <!--end of Top100 code-->
+		 
+         <!-- Yandex ТИЦ -->
+              <a href="http://www.yandex.ru/cy?base=0&host=www.telsi.ru">
+              <img src="http://www.yandex.ru/cycounter?www.telsi.ru" width="88" height="31" alt="Яндекс цитирования" border="0"></a>
+              <!-- Google PageRank-->
+              <a href=http://www.getrank.net/ target=_blank><img src=http://getrank.net/pr2.php alt=PageRank border=0></a>
+                                  
+<!--LiveInternet counter--><script type="text/javascript"><!--
+document.write("<a href='http://www.liveinternet.ru/click' "+
+"target=_blank><img src='http://counter.yadro.ru/hit?t54.1;r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";h"+escape(document.title.substring(0,80))+";"+Math.random()+
+"' alt='' title='LiveInternet: показано число просмотров и"+
+" посетителей за 24 часа' "+
+"border=0 width=88 height=31><\/a>")//--></script><!--/LiveInternet-->
+
+<!-- YaMetrics begin -->
+<script type="text/javascript"><!--
+var ya_cid=138018;
+//--></script>
+<script src="http://bs.yandex.ru/resource/watch.js" type="text/javascript"></script>
+<noscript><div style="display: inline;"><img src="http://bs.yandex.ru/watch/138018" width="1" height="1" alt=""></div></noscript>
+<!-- YaMetrics end -->
+
+<!-- RedHelper -->
+<script id="rhlpscrtg" type="text/javascript" charset="utf-8" async="async"
+	src="http://web.redhelper.ru/service/main.js?c=telsi"></script>
+<!--/Redhelper --></td>
+    </tr>
+
+  </tbody>
+</table>
+
+</div>
+
+
+<!-- Поисковая форма -->
+    <div class="frmSearch">
+     <form action="http://telsi.ru/search.php" method="GET">
+     <input type="search" name="srchtext" size="17" placeholder="Поиск товаров" value=""><input type="submit" value="Найти">
+     
+     </form>
+    </div>
+<!-- КОНЕЦ Поисковая форма -->
+
+
+
+<!--шапка-->
+<div class="head">
+
+<img border="0" src="design/telsi_shop/img/1_bg_head.jpg" width="1019" height="400">
+
+</div>
+
+<div class="head_sqare">
+
+</div>
+
+
+
+
+<div class="girl">
+
+<img border="0" src="design/telsi_shop/img/1_girl.gif" width="300" height="224">
+
+</div>
+
+
+<div class="head_logo">
+<img alt="Телси" border="0" src="design/telsi_shop/img/1_head_logo.gif" width="600" height="165">
+</div>
+
+<div class="head_mail">
+<a href="mailto:info@telsi.ru">
+<img alt="послать письмо в телси" border="0" src="design/telsi_shop/img/1_info@telsi.gif" width="210" height="24">
+</a>
+</div>
+
+<!--конец шапки-->
+
+<table cellpadding="0" cellspacing="0" background="design/telsi_shop/img/bg-hormenu.jpg"  width="100%" height="28" class="HorMenu">
+<tr>
+<td align="left">
+<nobr>&nbsp;
+<img src="design/telsi_shop/img/1_hmenu_razd.jpg" width="2" height="52" align="center" style="vertical-align:middle;"> 
+ &nbsp;<a href="index.php">Главная</a>&nbsp; 
+<img src="design/telsi_shop/img/1_hmenu_razd.jpg" width="2" height="52" align="center" style="vertical-align:middle;">
+
+ &nbsp;<a href="content/advantages.html">Наши преимущества</a>&nbsp; 
+<img src="design/telsi_shop/img/1_hmenu_razd.jpg" width="2" height="52" align="center" style="vertical-align:middle;">
+
+ &nbsp;<a href="content/about.html">О компании</a>&nbsp; 
+<img src="design/telsi_shop/img/1_hmenu_razd.jpg" width="2" height="52" align="center" style="vertical-align:middle;">
+
+ &nbsp;<a href="content/contacts.html">Контакты</a>&nbsp; 
+<img src="design/telsi_shop/img/1_hmenu_razd.jpg" width="2" height="52" align="center" style="vertical-align:middle;">
+
+ &nbsp;<a href="content/montag.html">Монтаж</a>&nbsp; 
+<img src="design/telsi_shop/img/1_hmenu_razd.jpg" width="2" height="52" align="center" style="vertical-align:middle;">
+
+ &nbsp;<a href="mailto:info@telsi.ru?subject=......:::::::WWW.TELSI.RU::::............">Почта</a>&nbsp; 
+<img src="design/telsi_shop/img/1_hmenu_razd.jpg" width="2" height="52" align="center" style="vertical-align:middle;">
+
+&nbsp;</nobr>
+</td>
+</tr>
+</table>
+
+
+
+
+
+</body>
+</html>
